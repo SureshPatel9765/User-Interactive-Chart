8351d7f9454ebd656c72a33d4fe30449a30b3378
SureshPatel9765 <psuresh629@gmail.com>
2025-05-19 02:07:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ st.title("📈 NSE Stock Analysis")
 
 # Input section
 
-user_input = st.text_input("Enter NSE symbol (e.g., TCS):", key="widget", on_change=submit).upper().strip()
+user_input = st.text_input("Enter NSE symbol (e.g., TCS):").upper().strip()
 selected_dropdown = st.selectbox("Or select from popular NSE stocks:", tickers)
 # Use typed input if available, otherwise dropdown
 selected = user_input if user_input else selected_dropdown
